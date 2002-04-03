cf3d3e641fc090ea6436ae2da2a78dccca5dcf7e
eric_schabell <>
2002-04-03 20:55:19 -0100

    Updated Mail-news section, postfix version upgrade to 1.1.7.

--- mail-news/postfix/postfix.sh
+++ mail-news/postfix/postfix.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 case $1 in
-  start|restart)  echo     "$1ing postfix MTA."
-                  postfix  stop  2>/dev/null
-                  newaliases
+          start)  echo     "$1ing postfix MTA."
                   postfix  start
                   ;;
            stop)  echo     "$1ing postfix"
                   postfix  stop
                   ;;
+	restart)
+		  $0 stop
+		  $0 start
+		  ;;  
               *)  echo     "Usage: $0 {start|stop|restart}"
                   ;;
 esac
