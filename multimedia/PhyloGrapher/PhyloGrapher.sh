bc4b5d02f4447f8d53ac83a6fdd96c5d7cfe3e75
Unet <unet@sourcemage.org>
2002-05-14 13:24:10 -0100

    Two new spells from my buddy, Eric Sandall :)

---
+++ multimedia/PhyloGrapher/PhyloGrapher.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Created by Eric Sandall <sandalle@yahoo.com>
+#
+# PhyloGrapher.sh  v1.0.0  2002-05-13  23:00 PST
+#
+# Runs PhyloGrapher_$VERSION_Linux.tcl in /usr/share/PhyloGrapher
+
+cd /usr/share/PhyloGrapher
+./PhyloGrapher_$VERSION\_Linux.tcl
