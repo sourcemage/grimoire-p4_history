6bd49d5709b1a58fc7e6a16cd70b86d12c40c61e
Justin Rocha <justin_rocha@vortex>
2002-07-31 04:48:37 -0100

    Persuing the man page, I found that you can reload the config file. How nice. ;-)

--- ftp/proftpd/proftpd.sh
+++ ftp/proftpd/proftpd.sh
@@ -8,6 +8,9 @@ case $1 in
               stop)  echo      "$1ing ProFTPd FTP server."
                      kill `cat /var/run/proftpd.pid`
                      ;;
-                 *)  echo      "Usage: $0 {start|stop|restart}"
+            reload)  echo      "$1ing ProFTPd FTP server."
+                     kill -HUP `cat /var/run/proftpd.pid`
+                     ;;
+                 *)  echo      "Usage: $0 {start|stop|restart|reload}"
                      ;;
 esac
