0561946eff8fd950414224052b516f6ebb2cb203
Eric Sandall <eric@sandall.us>
2002-08-22 05:05:31 -0100

    * Fixed Bug #392 (mount.sh)  * Added a dependency for init.d to provide /etc/init.d/functions

--- disk/e2fsprogs/mount.sh
+++ disk/e2fsprogs/mount.sh
@@ -1,51 +1,55 @@
 #!/bin/sh
+# /etc/init.d/mount.sh
+
+source /etc/init.d/functions
 
 start() {
 
   [  !  -d  /devices  ]  ||  mount   -n  -t  devfs devfs /devices
-
   mount   -n  -o  remount,ro  /
-
   echo    -n "Activating swap... "
   swapon  -a 2> /dev/null
-  echo    "done."
+  evaluate_retval
 
-  if  [ ! -e /fastboot ]; then 
+  if  [ ! -e /fastboot ]; then
 
     [ -e /forcefsck    ]  &&  FORCE="-f"
     [ "$FSCKFIX" = yes ]  &&  FIX="-y"  ||  FIX="-a"
 
     echo  -n "Checking file systems... "
     fsck  -C -A $FIX $FORCE
+    evaluate_retval
 
     if [ $? -gt 1 ];  then
+      $WARNING
       echo  "fsck failed."
       echo  "Please repair your file system"
       echo  "manually by running /sbin/fsck"
       echo  "without the -a option"
+      $NORMAL
       sulogin
       reboot  -f
     fi
   fi
-  echo     "done."
+
   mount    -n -o remount,rw /
   echo     > /etc/mtab
   mount    -f -o remount,rw /
-  mount    -a -F
+  echo "Mounting local filesystems..."
+  mount    -a
+  evaluate_retval
   rm       -f /fastboot /forcefsck
 
 }
 
-
 stop() {
-
-  echo     -n "Deactivating swap... "
+  echo      "Deactivating swap... "
   swapoff  -a
-  echo     "done."
+  evaluate_retval
 
-  echo     -n "Unmounting local filesystems... "
+  echo      "Unmounting local filesystems... "
   umount   -t nodevfs,noproc -f -a -r
-  echo     "done."
+  evaluate_retval
 
   cat  /etc/mtab  |
   while read DEVICE MOUNT REST; do
@@ -54,9 +58,11 @@ stop() {
 
   if  !  mount  -n -o remount,ro  /
   then
+    $WARNING
     echo  "/ failed to remount read only"
     read  -n 1  -t 120  -p  "Do you want to login? (y/n) "  CONFIRM
     echo
+    $NORMAL
     case  $CONFIRM in
       y|Y)  sulogin ;;
     esac
@@ -69,3 +75,5 @@ case "$1" in
    stop)  stop                            ;;
       *)  echo  "Usage: $0 {start|stop}"  ;;
 esac
+
+# end /etc/init.d/mount.sh
