5c0b60a98f2d4c328c1800108f4ea05e4c2e8063
Eric Sandall <eric@sandall.us>
2002-11-02 20:57:48 +0000

    * Updates for printer

---
+++ printer/cups/cups.no-functions.sh
@@ -0,0 +1,145 @@
+#!/bin/sh
+#
+# "$Id: cups.sh.in,v 1.21 2002/08/20 20:11:24 mike Exp $"
+#
+#   Startup/shutdown script for the Common UNIX Printing System (CUPS).
+#
+#   Copyright 1997-2002 by Easy Software Products, all rights reserved.
+#
+#   These coded instructions, statements, and computer programs are the
+#   property of Easy Software Products and are protected by Federal
+#   copyright law.  Distribution and use rights are outlined in the file
+#   "LICENSE.txt" which should have been included with this file.  If this
+#   file is missing or damaged please contact Easy Software Products
+#   at:
+#
+#       Attn: CUPS Licensing Information
+#       Easy Software Products
+#       44141 Airport View Drive, Suite 204
+#       Hollywood, Maryland 20636-3111 USA
+#
+#       Voice: (301) 373-9603
+#       EMail: cups-info@cups.org
+#         WWW: http://www.cups.org
+#
+
+
+#### OS-Dependent Information
+
+#
+#   Linux chkconfig stuff:
+#
+#   chkconfig: 235 99 00
+#   description: Startup/shutdown script for the Common UNIX \
+#                Printing System (CUPS).
+#
+
+
+#### OS-Dependent Configuration
+
+IS_ON=/bin/true
+#if test -f /etc/init.d/functions; then
+#	. /etc/init.d/functions
+#	ECHO=echo
+#	ECHO_OK="echo_success"
+#	ECHO_ERROR="echo_failure"
+#else
+	ECHO=echo
+	ECHO_OK=:
+	ECHO_ERROR=:
+#fi
+
+
+#### OS-Independent Stuff
+
+#
+# Set the timezone, if possible...  This allows the
+# scheduler and all child processes to know the local
+# timezone when reporting dates and times to the user. 
+# If no timezone information is found, then Greenwich
+# Mean Time (GMT) will probably be used.
+#
+
+for file in /etc/TIMEZONE /etc/rc.config /etc/sysconfig/clock; do
+	if test -f $file; then
+	        . $file
+	fi
+done
+
+if test "x$ZONE" != x; then
+	TZ="$ZONE"
+fi
+
+if test "x$TIMEZONE" != x; then
+	TZ="$TIMEZONE"
+fi
+
+if test "x$TZ" != x; then
+	export TZ
+fi
+
+
+#
+# See if the CUPS server (cupsd) is running...
+#
+
+pid=`ps ax | awk '{if (match($5, ".*/cupsd$") || $5 == "cupsd") print $1}'`
+
+
+#
+# Start or stop the CUPS server based upon the first argument to the script.
+#
+
+case $1 in
+	start | restart | reload)
+		if $IS_ON cups; then
+			if test "$pid" != ""; then
+				kill -HUP $pid
+			else
+				prefix=/usr
+				exec_prefix=/usr/bin
+				/usr/sbin/cupsd
+				if test $? != 0; then
+					$ECHO_FAIL
+					$ECHO "cups: unable to $1 scheduler."
+					exit 1
+				fi
+			fi
+			$ECHO_OK
+			$ECHO "cups: ${1}ed scheduler."
+		fi
+		;;
+
+	stop)
+		if test "$pid" != ""; then
+			kill $pid
+			$ECHO_OK
+			$ECHO "cups: stopped scheduler."
+		fi
+		;;
+
+	status)
+		if test "$pid" != ""; then
+			echo "cups: scheduler is running."
+		else
+			echo "cups: scheduler is not running."
+		fi
+		;;
+
+	*)
+		echo "Usage: cups {reload|restart|start|status|stop}"
+		exit 1
+		;;
+esac
+
+
+#
+# Exit with no errors.
+#
+
+exit 0
+
+
+#
+# End of "$Id: cups.sh.in,v 1.21 2002/08/20 20:11:24 mike Exp $".
+#
