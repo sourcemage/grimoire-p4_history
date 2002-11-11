54e0672f8822a89e1dd1d87516ee124fa7db8f42
neal_birch <>
2002-11-11 22:50:31 +0000

    Added localtime.sh to spell and updated the DETAILS and HISTORY.

---
+++ sgl/init.d/scriptdir/localtime.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# SGL-script-version=20021027
+# SGL-START:S:S15
+# SGL-STOP:0 6:K75
+
+. /etc/init.d/functions
+
+case $1 in 
+    start)
+	echo "Setting System Time using the Hardware Clock."
+	/sbin/hwclock  --hctosys  --localtime
+	evaluate_retval
+	;;
+    stop)
+	echo "Setting the Hardware Clock to the current System Time."
+	/sbin/hwclock --systohc
+	evaluate_retval
+	;;
+    adjust)
+	echo "Adjusting the Hardware Clock to account for drift."
+	/sbin/hwclock --adjust
+	evaluate_retval
+	;;
+    show)
+	echo "Show Hardware Clock time."
+	/sbin/hwclock --show
+	;;
+    *)
+	echo "Usage: $0 {start|stop|adjust|show}"
+	exit 1
+	;;
+esac
