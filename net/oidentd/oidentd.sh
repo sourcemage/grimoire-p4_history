afa72177805c370ccc416aa70502a9fc08b47d4f
Seth Woolley <seth@swoolley.homeip.net>
2003-01-30 18:03:17 +0000

    net md5 updates

--- net/oidentd/oidentd.sh
+++ net/oidentd/oidentd.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+source /etc/init.d/functions
+
 case  $1  in
-  start|restart)  echo  "$1ing oidentd."
-                  pkill  "^oidentd$"  &&  sleep 5
-                  oidentd  -u daemon  -g daemon
-                  ;;
-           stop)  echo  "$ping oidentd."
-                  pkill  "^oidentd$"
-                  ;;
-              *)  echo   "Usage: $0 {start|stop|restart}"
-                  ;;
+  start)    echo "Starting oidentd."
+            loadproc oidentd  -u daemon  -g daemon
+            ;;
+
+  restart)  $0 stop
+            $0 start
+            ;;
+
+  stop)     echo  "Stopping oidentd."
+            killproc oidentd
+            ;;
+
+  status)   statusproc oidentd
+            ;;
+
+  *)        echo   "Usage: $0 {start|stop|restart|status}"
+            ;;
 esac
