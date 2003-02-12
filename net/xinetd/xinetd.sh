21440915b9059950546f05d0fb606a0b126109f6
Eric Sandall <eric@sandall.us>
2003-02-12 17:49:20 +0000

    * xinetd patches from Robert

--- net/xinetd/xinetd.sh
+++ net/xinetd/xinetd.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
+#
+# Updated xinetd script
+# Changes:
+# 1. added function file
+# 2. split start / restart cases
+# 3. added status case
+
+source /etc/init.d/functions
 
 case  $1  in
-  start|restart)  echo   "$1ing Internet superserver, xinetd."
-                  pkill  "^xinetd$"  &&  sleep  3
-                  xinetd -reuse
-                  ;;
-           stop)  echo   "$1ping xinetd."
-                  pkill  "^xinetd$"
-                  ;;
-              *)  echo   "Usage: $0 {start|stop|restart}"
-                  ;;
+	start)
+		echo "Starting Internet superserver, xinetd..."
+		loadproc xinetd -reuse
+		;;
+	stop)
+		echo "Stopping xinetd..."
+		killproc xinetd
+		;;
+	restart)
+		echo "Restarting xinetd..."
+		$0 stop
+		/usr/bin/sleep 1
+		$0 start
+		;;
+	status)
+		statusproc xinetd
+		;;
+	*)
+		echo   "Usage: $0 {start|stop|restart|status}"
+		;;
 esac
