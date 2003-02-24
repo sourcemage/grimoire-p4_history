fc0a62c980a265758b0dcca0d4d542a2ba59d183
eric_schabell <>
2003-02-24 16:07:27 +0000

    Adam,      Here is an update to ensure the correct instalation of init scripts via librunlevel. 	 Just revert if you don't like it...

--- chat/silc-server/init.d/silcd.sh
+++ chat/silc-server/init.d/silcd.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
+#
+# SMGL-script-version=20030224
+# SMGL-START:1 2 3 4 5:S90
+# SMGL-STOP:0 6:K50
+
+. /etc/init.d/functions
+
 
 case $1 in
      start|restart)  echo   "$1ing silcd secure conferencer."
-                     pkill  "^silcd$"
-                     silcd
+                     killproc  silcd
+                     loadproc silcd
                      ;;
               stop)  echo   "$1ping silcd."
-                     pkill  "^silcd$"
+                     killproc silcd
                      ;;
                  *)  echo   "Usage: $0 {start|stop|restart}"
+				     exit 1
                      ;;
 esac
