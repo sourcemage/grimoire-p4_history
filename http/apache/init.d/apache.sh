746b383ead33108eec292b48a4e2c3acacbf2169
eric_schabell <>
2003-02-24 14:27:03 +0000

    Updated spell to use librunlevel for init.d functions.

---
+++ http/apache/init.d/apache.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# SMGL-script-version=20030224
+# SMGL-START:S 3 4 5:S90
+# SMGL-STOP:0 1 2 6:K10
+
+source /etc/init.d/functions
+
+case $1 in
+	  start)
+			echo "$1ing Apache web server"
+			apachectl $1
+			#	apachectl startssl
+			eval_retval
+			;;
+			
+  	restart)
+			echo "$1ing Apache web server."
+			if [ -f /var/run/httpd.pid ]; then
+				apachectl $1
+				eval_retval
+			else
+				$0 start
+			fi
+			;;
+			
+	   stop)
+			echo "$1ping Apache web server."
+			apachectl $1
+			eval_retval
+			;;
+			
+	      *)
+			echo "Usage: $0 {start|stop|restart}"
+			exit 1
+			;;
+esac
