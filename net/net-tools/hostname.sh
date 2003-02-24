0e8f5c114f36a22d83687b4659af736653b1e737
Eric Sandall <eric@sandall.us>
2003-02-24 16:20:29 +0000

    * Finished Robert's patches (properly, I hope)

--- net/net-tools/hostname.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SGL-script-version=20021024
-# set the above to custom instead of date format if you use
-# a custom hostname.sh script
-# this sets the run levels and priority for links
-# SGL-START:S:S20
-
-/bin/hostname  -F  /etc/hostname
