e148030c683d7866920cae32caafc070abf2db97
Eric Sandall <eric@sandall.us>
2003-03-04 17:55:52 +0000

    * init.d and TRIGGER cleanups  * new spell: changeip

--- net/bind/init.d/named.sh
+++ net/bind/init.d/named.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# SMGL-script-version=20030304
+# SMGL-START:3 4 5:S35
+# SMGL-STOP:0 1 2 6:K50
+
 case  $1  in
     start)  echo   "Starting Berkley Internet Name Domain"
             named  -u  bind                         ;;
