35cab39a0d26205c50b96f10738d07e78d600b12
mathieu_lubrano <>
2003-03-24 19:44:15 +0000

    - fixed bug #2898 - added sorcery init feature

--- mail/postfix/init.d/postfix.sh
+++ mail/postfix/init.d/postfix.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
 
+# SMGL-script-version=20030318
+# SMGL-START:3 4 5:S90
+# SMGL-STOP:0 1 2 6:K10
+
+. /etc/init.d/functions
+
 case $1 in
-   start)  echo     "$1ing postfix MTA."
+   start)  echo     "Starting postfix..."
 	   if [ -e /etc/aliases ]; then
 	     if [ -e /etc/aliases.db ] &&
 	        [ ""`/usr/bin/find /etc/aliases -follow -newer /etc/aliases.db` != "" ]; then
    	            echo "/etc/aliases is modified. You may want to run newaliases"
 	     fi
            fi
-           postfix  start
+           loadproc  postfix  start
            ;;
 
-    stop)  echo     "$1ing postfix"
+    stop)  echo     "Stoping postfix..."
            postfix  stop
+           loadproc true
            ;;
 
     restart)
 	   $0 stop
+           sleep 1
 	   $0 start
            ;;  
     reload)
 	   $0 restart
+           ;;    
+    status)
+           statusproc master
            ;;
-       *)  echo     "Usage: $0 {start|stop|restart|reload}"
+       *)  echo     "Usage: $0 {start|stop|restart|reload|status}"
            ;;
 esac
