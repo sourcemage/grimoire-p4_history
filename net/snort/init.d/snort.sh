0ea31acdaeb6bd9f7e6fffe96e6b97fabfbaf04c
Eric Sandall <eric@sandall.us>
2003-04-01 01:58:13 -0100

    * Removed execute bit

--- net/snort/init.d/snort.sh
+++ net/snort/init.d/snort.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Source Mage init.d install information
+# SMGL-START:3 4 5:S40
+# SMGL-STOP:0 1 2 6:K60
+#
 
 case $1 in
      start|restart)  echo   "$1ing Snort, Intrusion Detection System."
