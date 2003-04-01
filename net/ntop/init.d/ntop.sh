c1ae51126977cf5845bf523a1bdcf1e695244e77
Eric Sandall <eric@sandall.us>
2003-04-01 04:49:29 -0100

    * Removed execute bit and added init.d install information

--- net/ntop/init.d/ntop.sh
+++ net/ntop/init.d/ntop.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Source Mage init.d install information
+# SMGL-START:3 4 5:S90
+# SMGL-STOP:0 1 2 6:K10
+#
 
 case  $1  in
   start|restart)  echo   "$1ing ntop, a network traffic usage monitor."
