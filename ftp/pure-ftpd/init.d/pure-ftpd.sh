19ef6150ec382576aee2fb572287f26c68b6f82c
Eric Sandall <eric@sandall.us>
2003-04-01 03:44:59 -0100

    * Removed execute bit and added init.d information

--- ftp/pure-ftpd/init.d/pure-ftpd.sh
+++ ftp/pure-ftpd/init.d/pure-ftpd.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Source Mage init.d install information
+# SMGL-START:3 4 5:S90
+# SMGL-STOP:0 1 2 6:K10
+#
 
 if [ ! -e /etc/pure-ftpd.conf ]; then
   echo "Warning: /etc/pure-ftpd.conf missing..."
