5efba2c918b4224278666df55a79eb73cc3a0680
Eric Sandall <eric@sandall.us>
2003-05-01 06:03:55 -0100

    * Another devel -> test update

--- net/ntop/init.d/ntop.sh
+++ net/ntop/init.d/ntop.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 #
 # Source Mage init.d install information
-# SMGL-script-version=20030331
+# SMGL-script-version=20030418
 # SMGL-START:3 4 5:S90
 # SMGL-STOP:0 1 2 6:K10
 #
 
+source /etc/init.d/functions
+
 case  $1  in
-  start|restart)  echo   "$1ing ntop, a network traffic usage monitor."
-                  pkill  "^ntop$"
-                  ntop   -d
-                  ;;
-           stop)  echo   "$1ing ntop."
-                  pkill  "^ntop$"
-                  ;;
-              *)  echo   "Usage: $0 {start|stop|restart}"
-                  ;;
+  start)    loadproc ntop -d -u nobody -w 0 -W 8080 --use-syslog=daemon
+            ;;
+
+  restart)  $0 stop
+            $0 start
+            ;;
+
+  stop)     echo "Stopping ntop..."
+            killproc ntop
+            ;;
+
+  status)   statusproc ntop
+            ;;
+
+  *)        echo "Usage: $0 {start|stop|restart|status}"
+            ;;
 esac
