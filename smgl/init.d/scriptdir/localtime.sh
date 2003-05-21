007a5b24e0b6272e7dfdea5a87d76cd748a8cf59
Eric Sandall <eric@sandall.us>
2003-05-21 02:24:34 -0100

    * Re-integrated .sh files as sorcery doesn't have the code to handle the LFS filenames yet

---
+++ smgl/init.d/scriptdir/localtime.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# SMGL-script-version=20030224
+# SMGL-START:S:S15
+# SMGL-STOP:0 6:K75
+
+. /etc/init.d/functions
+
+case $1 in 
+    start)
+			echo  "Setting System Time using the Hardware Clock."
+			/sbin/hwclock  --hctosys  --localtime
+			evaluate_retval
+			;;
+    
+	stop)
+			echo  "Setting the Hardware Clock to the current System Time."
+			/sbin/hwclock --systohc --localtime
+			evaluate_retval
+			;;
+    
+	adjust)
+			echo  "Adjusting the Hardware Clock to account for drift."
+			/sbin/hwclock --adjust
+			evaluate_retval
+			;;
+			
+    show)
+			echo  "Show Hardware Clock time."
+			/sbin/hwclock --show
+			;;
+    *)
+			echo "Usage: $0 {start|stop|adjust|show}"
+			exit 1
+			;;
+esac
