6799f4ce25a2650fa42d39232596b213e833e6b5
robby_verkuylen <>
2003-05-27 20:03:40 -0100

    Use ${INSTALL_ROOT} for science section. Batch 1/2

--- science/PhyloGrapher/PhyloGrapher.sh
+++ science/PhyloGrapher/PhyloGrapher.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Created by Eric Sandall <sandalle@yahoo.com>
 #
-# PhyloGrapher.sh  v1.0.0  2002-05-13  23:00 PST
+# PhyloGrapher.sh  v1.1.0  2003-05-27
 #
-# Runs PhyloGrapher_$VERSION_Linux.tcl in /usr/share/PhyloGrapher
+# Runs PhyloGrapher_$VERSION_Linux.tcl in ${INSTALL_ROOT}/usr/share/PhyloGrapher
 
-cd /usr/share/PhyloGrapher
+cd $INSTALL_ROOT/usr/share/PhyloGrapher
 ./PhyloGrapher_$VERSION\_Linux.tcl
