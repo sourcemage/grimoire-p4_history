73aec5e572ef18f1fdea330f43ecbdcbaefa9e5b
Eric Sandall <eric@sandall.us>
2003-06-16 03:04:42 -0100

    * init script fix

--- net/bind/init.d/named.sh
+++ net/bind/init.d/named.sh
@@ -4,15 +4,25 @@
 # SMGL-START:3 4 5:S35
 # SMGL-STOP:0 1 2 6:K50
 
+. /etc/init.d/functions
+
 case  $1  in
     start)  echo   "Starting Berkley Internet Name Domain"
-            named  -u  bind                         ;;
+            loadproc named -u bind
+            ;;
 
-  restart)  rndc    stop
-            named  -u  bind                         ;;
+   reload)  echo   "Reloading Berkley Internet Name Domain"
+            reloadproc named SIGHUP
+            ;;
 
-     stop)  rndc   stop                             ;;
+  restart)  $0 stop
+            $0 start
+            ;;
 
-        *)  echo  "Usage: $0 {start|stop|restart}"  ;;
+     stop)  echo    "Stopping Berkley Internet Name Domain"
+            loadproc rndc stop
+            ;;
 
+        *)  echo  "Usage: $0 {start|stop|restart}"
+            ;;
 esac
