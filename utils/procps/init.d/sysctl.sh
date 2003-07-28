cc51072459a6f17e4109a1383cac26b88dc33bdb
Arwed von Merkatz <v.merkatz@gmx.net>
2003-07-28 21:13:25 -0100

    updated to new init system

--- utils/procps/init.d/sysctl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Source Mage init.d install information
-# SMGL-script-version=20030331
-# SMGL-START:S:S99
-# SMGL-STOP:6:K50
-#
-
-if  [  -f  /etc/sysctl.conf  ];  then  sysctl  -p;  fi
