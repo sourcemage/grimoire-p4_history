bdc484732e3d854a83c92f90e41cfcfe28f6e187
Seth Woolley <seth@swoolley.homeip.net>
2003-08-18 02:16:11 -0100

    init.d stuff for fun

--- smgl/init.d/init.d/mountall.sh
+++ smgl/init.d/init.d/mountall.sh
@@ -84,6 +84,16 @@ start()
   mount    -a  -t  nosmbfs,nonfs,noncpfs
   evaluate_retval
 
+  echo "Creating /var/run/utmp..."
+  [ -d /var/run ] && touch /var/run/utmp
+  evaluate_retval
+  echo "Writing dmesg log to /var/log/dmesg..."
+  [ -d /var/log ] && dmesg  >  /var/log/dmesg
+  evaluate_retval
+  echo "Making /tmp/.ICE-unix temporary directory..."
+  [ -d /tmp     ] && mkdir /tmp/.ICE-unix && chmod 1777 /tmp/.ICE-unix
+  evaluate_retval
+
   exit 0
 }
 
