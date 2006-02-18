b40479fefac3ca1cda60eca1a5dd8fb135e9b90b
andrew_levstik <>
2006-02-18 12:30:02 +0000

    Cherry-picked this to test...

--- shell-term-fm/bash/profile.d/lc.sh
+++ shell-term-fm/bash/profile.d/lc.sh
@@ -1,11 +1,71 @@
 #!/bin/bash
-# Added by /usr/sbin/smgl.install
-# First check if this variable is already set
-# then if not set, check it (maybe), then set it
+# Enhancing support for locale support
+# First source the locale config then
+# if a var is set set it...
 
-if  [ -z "$LANG" ] ; then
-        LANG="en_US"
+. /etc/sysconfig/locale
+
+if [[ -z $LANG ]]
+then
+  export LANG
 fi
 
-export LANG
+if [[ -z $LC_CTYPE ]]
+then
+  export LC_CTYPE
+fi
 
+if [[ -z $LC_NUMERIC ]]
+then
+  export LC_NUMERIC
+fi
+
+if [[ -z $LC_TIME ]]
+then
+  export LC_TIME
+fi
+
+if [[ -z $LC_COLLAT ]]
+then
+  export LC_COLLAT
+fi
+
+if [[ -z $LC_MONETARY ]]
+then
+  export LC_MONETARY
+fi
+
+if [[ -z $LC_MESSAGES ]]
+then
+  export LC_MESSAGES
+fi
+
+if [[ -z $LC_PAPER ]]
+then
+  export LC_PAPER
+fi
+
+if [[ -z $LC_NAME ]]
+then
+  export LC_NAME
+fi
+
+if [[ -z $LC_ADDRESS ]]
+then
+  export LC_ADDRESS
+fi
+
+if [[ -z $LC_TELEPHONE ]]
+then
+  export LC_TELEPHONE
+fi
+
+if [[ -z $LC_MEASUREMENT ]]
+then
+  export LC_MEASUREMENT
+fi
+
+if [[ -z $LC_ALL ]]
+then
+  export LC_ALL
+fi
